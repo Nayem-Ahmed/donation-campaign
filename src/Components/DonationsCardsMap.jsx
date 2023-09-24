4013da48c34532f286c20359d1ad12a4147cc8e4
nayem ahmed <freelancernayem.me@gmail.com>
2023-09-25 01:30:58 +0600

    Fix bug and update json file

--- src/Components/DonationsCardsMap.jsx
+++ src/Components/DonationsCardsMap.jsx
@@ -5,7 +5,7 @@ const DonationsCardsMap = () => {
     const donations = useLoaderData();
     return (
         <div>
-               <div className='grid lg:grid-cols-4 md:grid-cols-3 gap-4'>
+               <div className='grid lg:grid-cols-4 md:grid-cols-3 gap-4 mt-5'>
             {
                 donations.map(donation=> <DonationsCards key={donation.id} donatio={donation}></DonationsCards>)
             }
